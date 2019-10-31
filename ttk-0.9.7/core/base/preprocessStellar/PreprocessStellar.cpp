08b6be9ef0e684c7221afe130d9732c369725b52
Guoxi <liuguoxi@chd.edu.cn>
2019-10-31 16:09:05 -0400

    Create preprocessing module

---
+++ ttk-0.9.7/core/base/preprocessStellar/PreprocessStellar.cpp
@@ -0,0 +1,16 @@
+#include                  <PreprocessStellar.h>
+
+using namespace std;
+using namespace ttk;
+
+PreprocessStellar::PreprocessStellar(){
+
+  inputData_ = NULL;
+  outputData_ = NULL;
+  triangulation_ = NULL;
+}
+
+PreprocessStellar::~PreprocessStellar(){
+  
+}
+
