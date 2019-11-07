93d4a215f4808a9323fbef0dc445c6dba23ada3a
Guoxi <liuguoxi@chd.edu.cn>
2019-11-06 20:08:34 -0500

    Add octree class

---
+++ ttk-0.9.7/core/base/preprocessStellar/Octree.h
@@ -0,0 +1,269 @@
+#include <iostream>
+#include <assert.h>
+#include <unordered_map>
+#include <vector>
+#include <Triangulation.h>
+
+using namespace ttk;
+using namespace std;
+
+class OctreeNode
+{
+    public:
+        OctreeNode(){OctreeNode(0);}
+        OctreeNode(uint32_t location){
+            locCode = location; 
+            childExists = 0; 
+            vertexIds = new vector<SimplexId>();
+        }
+
+        ~OctreeNode(){}
+
+    protected:
+        uint32_t locCode;
+        uint8_t childExists;
+        vector<SimplexId>* vertexIds;
+
+    friend class Octree;
+};
+
+
+class Octree
+{
+    public:
+        Octree(Triangulation *t){
+            Octree(t, 1000);
+        }
+
+        Octree(Triangulation *t, uint32_t k){
+            OctreeNode root(1);
+//            cout << "root location code: " << root.locCode << endl;
+            allNodes[1] = root;
+            capacity = k;
+            triangulation_ = t;
+
+            // find the minimum and maximum coordinate values
+            float mins[3] = {FLT_MAX, FLT_MAX, FLT_MAX};
+            float maxs[3] = {FLT_MIN, FLT_MIN, FLT_MIN};
+            SimplexId vertexNum = t->getNumberOfVertices();
+            for(int i = 0; i < vertexNum; i++){
+                float coord[3];
+                t->getVertexPoint(i, coord[0], coord[1], coord[2]);
+                for(int j = 0; j < 3; j++){
+                    if(coord[j] < mins[j]) mins[j] = coord[j];
+                    if(coord[j] > maxs[j]) maxs[j] = coord[j];
+                }
+            }
+            
+            for(int j = 0; j < 3; j++){
+                center[j] = (mins[j] + maxs[j]) / 2.0;
+                size[j] = (maxs[j] - mins[j]) / 2.0;
+            }
+        }
+
+        ~Octree(){}
+
+        /**
+         * Return true if the octree is empty, otherwise false.
+         */
+        bool empty(){
+            OctreeNode *root = lookupNode(1);
+            if(root->vertexIds->size() == 0 && !root->childExists){
+                return true;
+            }
+
+            return false;
+        }
+
+        /**
+         * Get the depth of the octree.
+         */ 
+        size_t getNodeTreeDepth(const OctreeNode *node){
+            assert(node->locCode);
+            for(uint32_t lc=node->locCode, depth=0; lc!=1; lc>>=3, depth++)
+                return depth;
+        }
+
+        /**
+         * Get the parent node of the current node.
+         */ 
+        OctreeNode* getParentNode(OctreeNode *node){
+            const uint32_t locCodeParent = node->locCode >> 3;
+            return lookupNode(locCodeParent);
+        }
+
+        /**
+         * Traverse the octree from a given node.
+         */ 
+        void visitAll(OctreeNode *node){
+            for(int i = 0; i < 8; i++){
+                if(node->childExists & (1 << i)){
+                    const uint32_t locCodeChild = (node->locCode << 3) | i;
+                    const auto child = lookupNode(locCodeChild);
+                    visitAll(child);
+                }
+            }
+        }
+
+        /**
+         * Insert the vertex into the octree by its id.
+         */ 
+        void insertVertex(SimplexId &vertexId){
+            OctreeNode *current = lookupNode(1);
+            uint32_t location = current->locCode;
+            float ncenter[3], nsize[3];
+            for(int i = 0; i < 3; i++){
+                ncenter[i] = center[i];
+                nsize[i] = size[i];
+            }
+            
+            while(current != nullptr && current->childExists){
+                computeCenterSize(location, ncenter, nsize);
+                location = getChildLocation(location, vertexId, ncenter);
+                current = lookupNode(location);
+            }
+
+            if(current == nullptr){
+                OctreeNode newnode(location);
+                newnode.vertexIds = new vector<SimplexId>{vertexId};
+                allNodes[location] = newnode;
+            }else{
+                current->vertexIds->push_back(vertexId);
+                subdivide(current);
+            }
+        }
+
+
+    private:
+        Triangulation *triangulation_;
+        unordered_map<uint32_t, OctreeNode> allNodes;
+        uint32_t capacity;
+        float center[3];
+        float size[3];
+
+        /**
+         * Loop up the octree node in the unordered map given the location code.
+         * Returns nullptr if not found.
+         */
+        OctreeNode* lookupNode(uint32_t locCode){
+            const auto iter = allNodes.find(locCode);
+            return (iter == allNodes.end() ? nullptr : &(iter->second));
+        }
+
+
+        /**
+         * Computer the center and size with given location code.
+         * Note: initialize two arrays before calling this function!
+         */ 
+        void computeCenterSize(uint32_t location, float centerArr[3], float sizeArr[3]){
+            uint32_t tmp = location, leftmost = 0;
+            while(tmp >>= 1){
+                leftmost++;
+            }
+            if(leftmost % 3 != 0){
+                cerr << "computerCenterSize(): the location seems not correct!\n";
+                return;
+            }
+
+            uint32_t mask;
+            for(int i = leftmost-1; i >= 0; i-=3){
+                sizeArr[0] /= 2.0; sizeArr[1] /= 2.0; sizeArr[2] /= 2.0;
+                for(int j = 0; j < 3; j++){
+                    mask = 1 << (i-j);
+                    if(location & mask){
+                        centerArr[j] += sizeArr[j];
+                    }else{
+                        centerArr[j] -= sizeArr[j];
+                    }
+                }
+            }
+        }
+
+        /**
+         * Get the location code of the child.
+         */ 
+        uint32_t getChildLocation(uint32_t parLoc, SimplexId vertexId, float centerArr[3]){
+            float xval, yval, zval;
+            if(triangulation_->getVertexPoint(vertexId, xval, yval, zval)){
+                cout << "getChildLocation(): FAILED to get the coordinate values of the vertex id " << vertexId << endl;
+                return -1;
+            }
+
+            if(xval < centerArr[0]){
+                if(yval < centerArr[1]){
+                    if(zval < centerArr[2]){
+                        return (parLoc << 3);
+                    }else{
+                        return (parLoc << 3) + 1;
+                    }
+                }else{
+                    if(zval < centerArr[2]){
+                        return (parLoc << 3) + 2;
+                    }else{
+                        return (parLoc << 3) + 3;
+                    }
+                }
+            }else{
+                if(yval < centerArr[1]){
+                    if(zval < centerArr[2]){
+                        return (parLoc << 3) + 4;
+                    }else{
+                        return (parLoc << 3) + 5;
+                    }
+                }else{
+                    if(zval < centerArr[2]){
+                        return (parLoc << 3) + 6;
+                    }else{
+                        return (parLoc << 3) + 7;
+                    }
+                }
+            }
+
+            cout << "getChildLocation(): Shouldn't get here!\n";
+            return -1;
+        }
+
+        /**
+         * Recursive subdividing function to make sure each node does not exceed the capacity limit.
+         */ 
+        void subdivide(OctreeNode *node){
+            if(node == nullptr)
+                return;
+
+            if(node->vertexIds->size() > capacity){
+                uint32_t childCode = 0;
+                float ncenter[3], nsize[3];
+
+                // initialize the center and size arrays
+                for(int i = 0; i < 3; i++){
+                    ncenter[i] = center[i];
+                    nsize[i] = size[i];
+                }
+
+                computeCenterSize(node->locCode, ncenter, nsize);
+
+                for(int v : *(node->vertexIds)){
+                    childCode = getChildLocation(node->locCode, v, ncenter);
+                    OctreeNode *childNode = lookupNode(childCode);
+
+                    if(childNode == nullptr){
+                        OctreeNode newnode(childCode);
+                        newnode.vertexIds->push_back(v);
+                        allNodes[childCode] = newnode;
+                        node->childExists |= (1 << (childCode & 7));
+                    }else{
+                        childNode->vertexIds->push_back(v);
+                    }
+                }
+
+                delete node->vertexIds;
+                node->vertexIds = nullptr;
+
+                for(int i = 0; i < 8; i++){
+                    if(node->childExists & (1 << i)){
+                        subdivide(lookupNode((node->locCode << 3) | i));
+                    }
+                }
+            }
+        }
+};
